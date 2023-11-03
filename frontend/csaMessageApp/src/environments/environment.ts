b2e197d6ecda5ac25ea1b0b970d52fb4d4586c49
Niels Van De Ginste <niels.vandeginste@student.ap.be>
2023-11-03 20:16:51 +0100

    feat: add verification by OPA

--- frontend/csaMessageApp/src/environments/environment.ts
+++ frontend/csaMessageApp/src/environments/environment.ts
@@ -1,8 +1,18 @@
+// export const environment = {
+//     auth0_authority: 'https://flufap.eu.auth0.com',
+//     auth0_client_id: 'iysO4wHMr5oQF7V3F7gQX4Y8rJSHyCol',
+//     auth0_redirect_uri: 'https://jp3q26kr-4200.euw.devtunnels.ms/login/callback',
+//     auth0_post_logout_redirect_uri: 'https://jp3q26kr-4200.euw.devtunnels.ms',
+//     auth0_api_audience: 'flufapi',
+//     apiRoot: 'https://jp3q26kr-3000.euw.devtunnels.ms/',
+// };
+
+// Versie tunnels Niels
 export const environment = {
     auth0_authority: 'https://flufap.eu.auth0.com',
     auth0_client_id: 'iysO4wHMr5oQF7V3F7gQX4Y8rJSHyCol',
-    auth0_redirect_uri: 'https://jp3q26kr-4200.euw.devtunnels.ms/login/callback',
-    auth0_post_logout_redirect_uri: 'https://jp3q26kr-4200.euw.devtunnels.ms',
+    auth0_redirect_uri: 'https://c7wnrl4p-4200.euw.devtunnels.ms/login/callback',
+    auth0_post_logout_redirect_uri: 'https://c7wnrl4p-4200.euw.devtunnels.ms',
     auth0_api_audience: 'flufapi',
-    apiRoot: 'https://jp3q26kr-3000.euw.devtunnels.ms/',
+    apiRoot: 'https://c7wnrl4p-3000.euw.devtunnels.ms/',
 };
