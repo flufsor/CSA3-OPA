bea71974f4d7176768fc87c95148ea81df6d8f80
Tom Goedemé <flufsor@gmail.com>
2023-11-06 13:37:21 +0100

    remove angular environment files

---
+++ frontend/csaMessageApp/src/environments/environment.example.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    auth0_authority: '', // The authority URL for the Auth0 authentication service.
+    auth0_client_id: '', // The client ID for the Auth0 application.
+    auth0_redirect_uri: '', // The redirect URI after successful authentication.
+    auth0_post_logout_redirect_uri: '', // The URI to redirect to after logging out.
+    auth0_api_audience: '', // The audience or target API for Auth0 (e.g., API endpoints).
+
+    apiRoot: '', // The root URL for the application's backend API.
+};
--- frontend/csaMessageApp/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    auth0_authority: 'https://flufap.eu.auth0.com',
-    auth0_client_id: 'iysO4wHMr5oQF7V3F7gQX4Y8rJSHyCol',
-    auth0_redirect_uri: 'http://localhost:4200/login/callback',
-    auth0_post_logout_redirect_uri: 'http://localhost:4200',
-    auth0_api_audience: 'flufapi',
-    apiRoot: 'http://localhost:3000/',
-};
